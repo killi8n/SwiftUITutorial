bc61389bbf0afe7c49cbe0acf2c84c42bd4afcb1
killi8n <killi8n.dev@gmail.com>
2021-01-12 11:41:36 +0900

    feat: step2

--- SwiftUITutorial/Views/CircleImage.swift
+++ SwiftUITutorial/Views/CircleImage.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
     var body: some View {
-        Image("Big_Sur")
+        image
             .resizable()
             .scaledToFit()
             .clipShape(Circle())
@@ -20,6 +21,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
---
+++ SwiftUITutorial/Views/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  SwiftUITutorial
+//
+//  Created by Dongho Choi on 2021/01/12.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
